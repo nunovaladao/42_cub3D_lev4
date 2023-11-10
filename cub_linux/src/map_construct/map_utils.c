f09420d00d181e354a83a40f039650188a299728
nunovaladao <utilizador@MacBook-Pro-Valadao.local>
2023-11-10 10:53:42 +0000

    fix norm

--- cub_linux/src/map_construct/map_utils.c
+++ cub_linux/src/map_construct/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: nsoares- <nsoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 17:51:28 by nsoares-          #+#    #+#             */
-/*   Updated: 2023/11/10 10:36:38 by nsoares-         ###   ########.fr       */
+/*   Updated: 2023/11/10 10:53:09 by nsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ void	free_map(t_map *m)
 	int	i;
 
 	i = -1;
-	if (m->n_texture)
+	/* if (m->n_texture)
 		free(m->n_texture);
 	if (m->s_texture)
 		free(m->s_texture);
 	if (m->w_texture)
 		free(m->w_texture);
 	if (m->e_texture)
-		free(m->e_texture);
+		free(m->e_texture); */
 	if (m->worldmap)
 	{
 		while (++i < m->nr_rows)
